48d95ae4d1e1e5cc7b797e56f5b44274c67c98a1
josecatanao <51245690+josecatanao@users.noreply.github.com>
2025-07-22 12:03:08 +0000

    mais atividades

---
+++ aula7.js
@@ -0,0 +1,15 @@
+let num1 = 9.546789;
+let num2 = Math.floor(num1);
+let num3 = Math.round(num1);
+let num4 = Math.ceil(num1);
+
+console.log(num2)
+console.log(num3)
+console.log(num4)
+
+
+console.log(Math.max(1,2,3,4,5,6,7,8,9,5,3,2,4,6,4,2,8,9,5,4,4674,33))
+console.log(Math.min(1,2,3,4,5,6,7,8,9,5,3,2,4,6,4,2,8,9,5,4,4674,33))
+
+const aleatorio =  Math.round( Math.random() * (10-5) + 5)
+console.log(aleatorio)
---
+++ aula8.js
@@ -0,0 +1,18 @@
+const alunos = ['José', 'Pedro','Lucas'];
+
+
+alunos.push('Otavio');
+alunos.unshift('pedro')
+alunos.pop()
+
+// alunos[2] = 'erick';
+// alunos[3] = 'Luiza';
+
+console.log(alunos.length)
+console.log(alunos);
+console.log(alunos.reverse())
+
+
+
+console.log(alunos[0]);
+console.log(alunos[2]);
